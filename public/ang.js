397e765d2109c14cb78d91c5846419b592b874d8
soutchay <chungvus@gmail.com>
2015-01-15 15:18:13 -0800

    placeholder style for main page

--- public/ang.js
+++ public/ang.js
@@ -1,7 +1,14 @@
 angular.module('myApp', [])
 .controller('myController', function($scope){
+	$scope.appliedStyle = "hide";
 	$scope.clicked = function(){
 		console.log('clicked');
-	}
-	$scope.style = "test";
-})
+		$scope.appliedStyle = "";
+	};
+	$scope.projects = [
+	{name: "LivingRoom", link: "https://livingroom.herokuapp.com/", image: "image here"},
+	{name: "Yabe", link: "https://livingroom.herokuapp.com/", image: "image here"},
+	{name: "C*Splat", link: "https://csplat.herokuapp.com/", image: "image here"},
+	{name: "TicTacToe", link: "https://tictacyo.firebaseapp.com/", image: "image here"},
+	];
+});
