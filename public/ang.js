0963e95ef2113fa33f6549f15467881bfaa3340b
soutchay <chungvus@gmail.com>
2015-01-20 22:44:37 -0800

    make div link to project

--- public/ang.js
+++ public/ang.js
@@ -1,5 +1,5 @@
 angular.module('myApp', [])
-.controller('myController', function($scope, $timeout){
+.controller('myController', function($scope, $timeout, $location){
 	$scope.appliedStyle = "hide";
 	$scope.clicked = function(){
 		console.log(this);
@@ -7,9 +7,10 @@ angular.module('myApp', [])
 			$scope.appliedStyle = "";
 		}, 1500);
 	};
+	
 	$scope.projects = [
 	{name: "LivingRoom", link: "https://livingroom.herokuapp.com/", image: "image here"},
-	{name: "Yabe", link: "https://livingroom.herokuapp.com/", image: "image here"},
+	{name: "Yabe", link: "https://itsyabe.herokuapp.com/", image: "image here"},
 	{name: "C*Splat", link: "https://csplat.herokuapp.com/", image: "image here"},
 	{name: "TicTacToe", link: "https://tictacyo.firebaseapp.com/", image: "image here"},
 	];
